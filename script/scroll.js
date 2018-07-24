0bcc00b8b3cf26412d4582c4be1ce61bb9f10d3f
calinvlad <calinvlad.t@gmail.com>
2018-07-24 05:04:24 +0300

    05:00

---
+++ script/scroll.js
@@ -0,0 +1,11 @@
+$('#contactTarget').click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Contact").offset().top
+    }, 2500)
+})
+
+$('#portofoliuTarget').click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Portofoliu").offset().top
+    }, 2500)
+})
