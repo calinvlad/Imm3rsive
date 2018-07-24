cc2937775e785acf1069a12eac03ab731a796afa
calinvlad <calinvlad.t@gmail.com>
2018-07-24 09:56:59 +0300

    navigation

--- script/scroll.js
+++ script/scroll.js
@@ -4,8 +4,32 @@ $('#contactTarget').click(function() {
     }, 2500)
 })
 
+$('#contactTargetBtn').click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Contact").offset().top
+    }, 2500)
+})
+
 $('#portofoliuTarget').click(function() {
     $('html, body').animate({
         scrollTop: $("#Portofoliu").offset().top
+    }, 2500)
+})
+
+$('#despreTarget').click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Despre").offset().top
+    }, 2500)
+})  
+
+$('#serviciiTarget').click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Servicii").offset().top
+    }, 2500)
+})  
+
+$('#homeTarget').click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Home").offset().top
     }, 2500)
 })
